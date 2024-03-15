9c8743a45ac24b52f322f2c31195173388d8c34b
takuto <tiharu717@gmail.com>
2024-03-15 18:42:33 +0900

    [add] StaggeredGrid

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+LeaningItem content1 = LeaningItem(
+  imageWidget: Image.network('https://picsum.photos/1920/1080'),
+  tags: [Tag('Flutter', Colors.blue), Tag('Dart', Colors.blue)],
+  progress: Progress.notStarted,
+  date: LeaningItemDate(2022, 12),
+  title: 'Learning Flutter',
+  description:
+      'これは学習アイテムカードです。その目的は、学習アイテムをカード形式で表示することです。タイトル、説明、画像があります。また、タグ、進行状況、日付もあります。タグは、学習アイテムのタグを表示するために使用されます。',
+);
+LeaningItem content2 = LeaningItem(
+  imageWidget: Image.network('https://picsum.photos/1920/1080'),
+  tags: [Tag('React', Colors.red), Tag('JavaScript', Colors.yellow)],
+  progress: Progress.inProgress,
+  date: LeaningItemDate(2022, 12),
+  title: 'Learning React',
+  description:
+      'これは学習アイテムカードです。その目的は、学習アイテムをカード形式で表示することです。タイトル、説明、画像があります。また、タグ、進行状況、日付もあります。タグは、学習アイテムのタグを表示するために使用されます。このカードは、ユーザーが学習アイテムの詳細情報にアクセスできるようにするために使用されます。ユーザーは、タイトル、説明、画像、タグ、進行状況、日付などの情報を確認できます。さらに、ユーザーは学習アイテムの進行状況を更新することもできます。このカードは、学習アプリや教育アプリなどのアプリケーションで広く使用されています。',
+);
+LeaningItem content3 = LeaningItem(
+  imageWidget: Image.network('https://picsum.photos/1920/1080'),
+  tags: [Tag('Python', Colors.green), Tag('Django', Colors.green)],
+  progress: Progress.completed,
+  date: LeaningItemDate(2022, 12),
+  title: 'Learning Python',
+  description:
+      'これは学習アイテムカードです。その目的は、学習アイテムをカード形式で表示することです。タイトル、説明、画像があります。また、タグ、進行状況、日付もあります。タグは、学習アイテムのタグを表示するために使用されます。このカードは、ユーザーが学習アイテムの詳細情報にアクセスできるようにするために使用されます。ユーザーは、タイトル、説明、画像、タグ、進行状況、日付などの情報を確認できます。さらに、ユーザーは学習アイテムの進行状況を更新することもできます。このカードは、学習アプリや教育アプリなどのアプリケーションで広く使用されています。',
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,44 +41,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        body: Row(
-          children: [
-            Spacer(),
-            Flexible(
-              flex: 1,
-              child: LearningItemCard(
-                leaningItem: LeaningItem(
-                  imageWidget: Image.network('https://picsum.photos/1920/1080'),
-                  tags: [Tag('Flutter', Colors.blue), Tag('Dart', Colors.blue)],
-                  progress: Progress.notStarted,
-                  date: LeaningItemDate(2022, 12),
-                  title: 'Learning Flutter',
-                  description:
-                      'これは学習アイテムカードです。その目的は、学習アイテムをカード形式で表示することです。タイトル、説明、画像があります。また、タグ、進行状況、日付もあります。タグは、学習アイテムのタグを表示するために使用されます。',
-                ),
-              ),
-            ),
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      ),
-    );
+        home: Scaffold(
+            body: GridView.custom(
+                gridDelegate: SliverWovenGridDelegate.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                    pattern: [
+                      WovenGridTile(3 / 5),
+                      WovenGridTile(1),
+                    ]),
+                childrenDelegate: SliverChildBuilderDelegate(
+                  (context, index) => index.isOdd
+                      ? LearningItemCard(leaningItem: content1)
+                      : LearningItemCard(leaningItem: content2),
+                ))));
+  }
+}
+
+class LearningItemList extends StatelessWidget {
+  const LearningItemList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemCount: 1,
+        itemBuilder: (BuildContext context, int index) {
+          return LearningItemCard(
+              leaningItem: index.isEven ? content1 : content2);
+        });
   }
 }
 
 class LearningItemCard extends StatelessWidget {
   final LeaningItem leaningItem;
-  const LearningItemCard({super.key, required this.leaningItem});
+  final double? width;
+  const LearningItemCard({
+    super.key,
+    required this.leaningItem,
+    this.width,
+  });
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       return Container(
+        width: this.width ?? constraints.maxWidth,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(constraints.maxWidth / 18),
@@ -62,23 +110,20 @@ class LearningItemCard extends StatelessWidget {
           ],
         ),
         child: Column(mainAxisSize: MainAxisSize.min, children: [
-          LayoutBuilder(builder: (context, constraintsssssss) {
-            //tip AspectiRatioはheightがinfiniteの場合、widthを基準にしてくれる
-            return AspectRatio(
-                aspectRatio: 16 / 9,
-                child: LayoutBuilder(builder: (context, constraintsss) {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Flexible(
-                          child: ClipRRect(
-                              child: leaningItem.imageWidget,
-                              borderRadius: BorderRadius.circular(
-                                  constraintsss.maxWidth / 18))),
-                    ],
-                  );
-                }));
-          }),
+          AspectRatio(
+              aspectRatio: 16 / 9,
+              child: LayoutBuilder(builder: (context, constraintsss) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                        child: ClipRRect(
+                            child: leaningItem.imageWidget,
+                            borderRadius: BorderRadius.circular(
+                                constraintsss.maxWidth / 18))),
+                  ],
+                );
+              })),
           LayoutBuilder(builder: (context, constraints) {
             return Padding(
               padding: const EdgeInsets.all(15.0),
@@ -93,13 +138,15 @@ class LearningItemCard extends StatelessWidget {
                   ),
                   SizedBox(height: constraints.maxWidth / 30),
                   Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: SelectableText(
-                      textAlign: TextAlign.center,
-                      leaningItem.description,
-                      style: TextStyle(fontSize: constraints.maxWidth / 30),
-                    ),
-                  ),
+                      padding: const EdgeInsets.all(5.0),
+                      // child: SelectableText(
+                      //   textAlign: TextAlign.center,
+                      //   leaningItem.description,
+                      //   style: TextStyle(fontSize: constraints.maxWidth / 30),
+                      child: Text(
+                        leaningItem.description,
+                        style: TextStyle(fontSize: constraints.maxWidth / 30),
+                      )),
                   Padding(
                     padding: EdgeInsets.all(constraints.maxWidth / 25),
                     child: Wrap(
@@ -116,29 +163,29 @@ class LearningItemCard extends StatelessWidget {
                           .toList(),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: constraints.maxWidth / 35,
-                        right: constraints.maxWidth / 35,
-                        bottom: constraints.maxWidth / 35),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: SelectableText(
-                                textAlign: TextAlign.start,
-                                leaningItem.progress.toString().split('.').last,
-                                style: TextStyle(
-                                    fontSize: constraints.maxWidth / 30))),
-                        Expanded(
-                          child: SelectableText(
-                              textAlign: TextAlign.end,
-                              '${leaningItem.date.year}/${leaningItem.date.month}',
-                              style: TextStyle(
-                                  fontSize: constraints.maxWidth / 30)),
-                        ),
-                      ],
-                    ),
-                  ),
+                  // Padding(
+                  //   padding: EdgeInsets.only(
+                  //       left: constraints.maxWidth / 35,
+                  //       right: constraints.maxWidth / 35,
+                  //       bottom: constraints.maxWidth / 35),
+                  //   child: Row(
+                  //     children: [
+                  //       Expanded(
+                  //           child: SelectableText(
+                  //               textAlign: TextAlign.start,
+                  //               leaningItem.progress.toString().split('.').last,
+                  //               style: TextStyle(
+                  //                   fontSize: constraints.maxWidth / 30))),
+                  //       Expanded(
+                  //         child: SelectableText(
+                  //             textAlign: TextAlign.end,
+                  //             '${leaningItem.date.year}/${leaningItem.date.month}',
+                  //             style: TextStyle(
+                  //                 fontSize: constraints.maxWidth / 30)),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
                 ],
               ),
             );
