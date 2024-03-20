dbd0c97a66e078ea9b59150dc704391b5fda678e
takuto <tiharu717@gmail.com>
2024-03-20 14:48:06 +0900

    sort out

---
+++ lib/custom_appbar.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double offset;
+  const CustomAppBar({super.key, required this.offset});
+
+  static const max = 255 * 10;
+  int _calculateOpacity(double offset) {
+    if (offset < 0 || offset > max) {
+      return 0;
+    } else {
+      return 255 - (255 * (offset / max)).toInt();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('offset: $offset');
+    return LayoutBuilder(builder: (context, constraints) {
+      return Container(
+        constraints: const BoxConstraints.expand(),
+        color: Color.fromARGB(_calculateOpacity(offset), 255, 255, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: const Text("活動成果"),
+              onPressed: () {},
+            ),
+            Text('制約はheight: ${constraints.maxHeight}, width: ${constraints.maxWidth}です。, '),
+          ],
+        ),
+      );
+    });
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+}
--- lib/learning_item.dart
+++ lib/learning_item.dart
@@ -1,59 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:portfolio/custom_appbar.dart';
-import 'package:portfolio/learning_item.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class LearningItemList extends StatelessWidget {
+  final List<LearningItem> learningItems;
+  const LearningItemList({super.key, required this.learningItems});
 
-
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  SingleChildScrollView scrollController = const SingleChildScrollView();
-  double scrollOffset = 0;
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Scaffold(
-          appBar: CustomAppBar(offset: scrollOffset),
-          body: LayoutBuilder(
-            builder: (context, constraints) {
-              return NotificationListener(
-                onNotification: (notification) {
-                  if (notification is UserScrollNotification) {
-                    setState(() {
-                      scrollOffset = notification.metrics.pixels;
-                    });
-                  }
-                  return false;
-                },
-                child: SingleChildScrollView(
-                  controller: scrollController.controller,
-                  child: LearningItemList(learningItems: [content1, content2, content3, content1],)                ),
-              );
-            },
-          ),
-        ));
-  }
+    return Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: StaggeredGrid.count(crossAxisCount: 2,
+       mainAxisSpacing: 25, 
+       crossAxisSpacing: 25,  children: learningItems.map((learningItem) => LearningItemCard(leaningItem: learningItem)).toList()),
+    );
+     }
 }
-
-//
-// }
-
 
 class LearningItemCard extends StatelessWidget {
   final LearningItem leaningItem;
@@ -131,29 +92,6 @@ class LearningItemCard extends StatelessWidget {
                             .toList(),
                       ),
                     ),
-                    // Padding(
-                    //   padding: EdgeInsets.only(
-                    //       left: constraints.maxWidth / 35,
-                    //       right: constraints.maxWidth / 35,
-                    //       bottom: constraints.maxWidth / 35),
-                    //   child: Row(
-                    //     children: [
-                    //       Expanded(
-                    //           child: SelectableText(
-                    //               textAlign: TextAlign.start,
-                    //               leaningItem.progress.toString().split('.').last,
-                    //               style: TextStyle(
-                    //                   fontSize: constraints.maxWidth / 30))),
-                    //       Expanded(
-                    //         child: SelectableText(
-                    //             textAlign: TextAlign.end,
-                    //             '${leaningItem.date.year}/${leaningItem.date.month}',
-                    //             style: TextStyle(
-                    //                 fontSize: constraints.maxWidth / 30)),
-                    //       ),
-                    //     ],
-                    //   ),
-                    // ),
                   ],
                 ),
               );
@@ -207,3 +145,29 @@ enum Progress {
   inProgress,
   completed,
 }
+LearningItem content1 = LearningItem(
+  imageWidget: Image.network('https://picsum.photos/1920/1080'),
+  tags: [Tag('Flutter', Colors.blue), Tag('Dart', Colors.blue)],
+  progress: Progress.notStarted,
+  date: LeaningItemDate(2022, 12),
+  title: 'Learning Flutter',
+  description: 'これは学習アイテムカードです。その目的は、学習アイテムをカード形式で表示することです。タイトル、説明、画像があります。また、タグ、進行状況、日付もあります。タグは、学習アイテムのタグを表示するために使用されます。',
+);
+LearningItem content2 = LearningItem(
+  imageWidget: Image.network('https://picsum.photos/1080/1920'),
+  tags: [Tag('React', Colors.red), Tag('JavaScript', Colors.yellow)],
+  progress: Progress.inProgress,
+  date: LeaningItemDate(2022, 12),
+  title: 'Learning React',
+  description:
+      'これは学習アイテムカードです。その目的は、学習アイテムをカード形式で表示することです。タイトル、説明、画像があります。また、タグ、進行状況、日付もあります。タグは、学習アイテムのタグを表示するために使用されます。このカードは、ユーザーが学習アイテムの詳細情報にアクセスできるようにするために使用されます。ユーザーは、タイトル、説明、画像、タグ、進行状況、日付などの情報を確認できます。さらに、ユーザーは学習アイテムの進行状況を更新することもできます。このカードは、学習アプリや教育アプリなどのアプリケーションで広く使用されています。',
+);
+LearningItem content3 = LearningItem(
+  imageWidget: Image.network('https://picsum.photos/1920/1080'),
+  tags: [Tag('Python', Colors.green), Tag('Django', Colors.green)],
+  progress: Progress.completed,
+  date: LeaningItemDate(2022, 12),
+  title: 'Learning Python',
+  description:
+      'これは学習アイテムカードです。その目的は、学習アイテムをカード形式で表示することです。タイトル、説明、画像があります。また、タグ、進行状況、日付もあります。タグは、学習アイテムのタグを表示するために使用されます。このカードは、ユーザーが学習アイテムの詳細情報にアクセスできるようにするために使用されます。ユーザーは、タイトル、説明、画像、タグ、進行状況、日付などの情報を確認できます。さらに、ユーザーは学習アイテムの進行状況を更新することもできます。このカードは、学習アプリや教育アプリなどのアプリケーションで広く使用されています。',
+);
